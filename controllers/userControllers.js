c226d452a61761a56cbf62a37d370c1ec0ebfa41
osman nasirdeen <nasirdeenosman@gmail.com>
2023-10-09 06:52:45 +0000

    new file:   auth/auth.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const userModel = require("../models/user");
 
 const getAllUsers = async (req, res) => {
@@ -21,9 +22,16 @@ const getUser = async (req, res) => {
       });
     }
     if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.JWT_TOKEN_KEY,
+        {
+          expiresIn: "5h",
+        }
+      );
       return res
         .status(200)
-        .json({ user, success: true, msg: "log in successful" });
+        .json({ user, success: true, msg: "log in successful", token });
     }
     res.status(401).json({ success: false, msg: "invalid password" });
   } catch (error) {
@@ -37,12 +45,10 @@ const addUser = async (req, res) => {
   const existingUser = await userModel.findOne({ email });
 
   if (existingUser) {
-    return res
-      .status(401)
-      .json({
-        success: false,
-        msg: "User With This Email Already Exist. Please Login",
-      });
+    return res.status(401).json({
+      success: false,
+      msg: "User With This Email Already Exist. Please Login",
+    });
   }
   try {
     const user = await userModel.create({
@@ -51,10 +57,18 @@ const addUser = async (req, res) => {
       email,
       password: encryptedUserPassword,
     });
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.JWT_TOKEN_KEY,
+      {
+        expiresIn: "5h",
+      }
+    );
     res.status(201).json({
       success: true,
       status: "user successfully added to database",
       user: user,
+      token,
     });
   } catch (error) {
     res.status(500).json({ message: error, success: false });
